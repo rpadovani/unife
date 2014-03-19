2dfe26b3259289c012a37707f2291e6ad90be77f
Riccardo Padovani <riccardo@rpadovani.com>
2014-03-19 14:14:43 +0100

    Updated swam.asm

--- asm/swap.asm
+++ asm/swap.asm
@@ -1,7 +1,15 @@
 swap:
-	lw $t0, 0($a0)	# Carica l in t0
-	lw $t1, 0($a1)	# Carica r in t1
-	sw $t0, 0($a1)	# Salva t0 in r
-	sw $t1, 0($a0)	# Salva t1 in l
+	addiu $sp, $sp, -8
+	sw $s0, 0($sp)	# Salvo s0
+	sw $s1, 4($sp)	# Salvo s1
+
+	lw $s0, 0($a0)	# s0 = *l
+	lw $s1, 0($a1)	# si = *r
+	sw $s0, 0($a1)	# *r = s0
+	sw $s1, 0($a0)	# *l = s1
+
+	lw $s0, 0($sp)	# Ripristino s0
+	lw $s1, 4($sp)	# Ripristino s1
+	addiu $sp, $sp, 8
 
 	jr $ra			# Ritorna il controllo al chiamante

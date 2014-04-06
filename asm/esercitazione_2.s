0c03d181081ff698fe2a097b48740a4f0153c563
puskolon <puskolon@gmail.com>
2014-04-06 14:32:27 +0200

    cambiati i registri

--- asm/esercitazione_2.s
+++ asm/esercitazione_2.s
@@ -4,23 +4,23 @@
 
 n_prime:
 
-	move $s2, $a0
-	addi $s0, $zero, 2						# numero
-	addi $s1, $zero, 2						# i = 1
+	move $t4, $a0
+	or $t5, $zero, 2						# numero
+	or $t6, $zero, 2						# i = 1
 	or $t1, $zero, $zero 					# resto = 0 
 	or $t2, $zero, $zero					# flag = 0
 	or $t3, $zero, 1						# lista_primi = 1 ( l' 1 è un numero primo )
-	beq $s0, 2, INCR_PRIME_LIST
+	beq $t5, 2, INCR_PRIME_LIST
 
 CICLO: 
 
-	beq $s0, $s1, CONTROL_FLAG				# se numero == i, va al controllo della flag
-	div $s0, $s1 							# divido il numero per l' indice 
+	beq $t5, $t6, CONTROL_FLAG				# se numero == i, va al controllo della flag
+	div $t5, $t6 							# divido il numero per l' indice 
 	mfhi $t1								# copio il resto in $t1
 	bne $t1, $zero, INCR_INDEX				# se resto != 0, incrementa l'indice
 	addi $t2, $t2, 1						# altrimenti incremento la flag
-	addi $s0, $s0, 1						# incremento il numero
-	addi $s1, $zero, 2						# e reinizializzo i a 2
+	addi $t5, $t5, 1						# incremento il numero
+	addi $t6, $zero, 2						# e reinizializzo i a 2
 	or $t2, $zero, $zero					# resetto la flag a 0
 
 	j CICLO
@@ -28,23 +28,23 @@ CICLO:
 CONTROL_FLAG:
 
 	beq $t2, $zero, INCR_PRIME_LIST			# se il numero è primo, salta a INCR_PRIME_LIST
-	addi $s0, $s0, 1						# incremento il numero
+	addi $t5, $t5, 1						# incremento il numero
 	j CICLO
 
 INCR_PRIME_LIST:
 	
 	addi $t3, $t3, 1						# incremento la lista dei primi
-	beq $t3, $s2, S_END						# se la lista è arrivata al numero richesto, va a S_END
-	addi $s0, $s0, 1						# incremento il numero
-	addi $s1, $zero, 2						# reinizializzo i a 2
+	beq $t3, $t4, S_END						# se la lista è arrivata al numero richesto, va a S_END
+	addi $t5, $t5, 1						# incremento il numero
+	addi $t6, $zero, 2						# reinizializzo i a 2
 	j CICLO
 
 INCR_INDEX:
 	
-	addi $s1, $s1, 1						# incrementa l' indice
+	addi $t6, $t6, 1						# incrementa l' indice
 	j CICLO
 
 S_END:
 
-	move $v0, $s0							# stampa il numero richiesto
+	move $v0, $t5							# copia il numero richiesto nel registro v0
 	jr $ra
